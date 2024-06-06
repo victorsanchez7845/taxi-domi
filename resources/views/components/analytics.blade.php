c27b3f14bf498c3d33d31be428ce7c7ab9d1ea79
caribbean-transfers <development@caribbean-transfers.com>
2024-06-06 13:13:43 -0500

    [UPDATE] Se agrega código de Analytics

---
+++ resources/views/components/analytics.blade.php
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16596385055"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-16596385055');
+</script>
