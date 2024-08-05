af393d32cd373dff1039cb3f64dc9771e1d4ace7
caribbean-transfers <development@caribbean-transfers.com>
2024-08-05 15:38:05 -0500

    [UPDATE] Se agrega tag manager
    Se agrega tag manager

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -46,6 +46,14 @@
 
         @stack('push-top')
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16596385055"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16596385055');
+        </script>
+        
         <script>
             setTimeout(function () {
                 window.__lc = window.__lc || {};
